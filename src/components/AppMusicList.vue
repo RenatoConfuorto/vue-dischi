07486bde7011c9cb5f63df1903537cdb1c1fd46a
RenatoConfuorto <renato.confuorto@outlook.it>
2022-05-19 19:32:07 +0200

    Bonus, componente loader

--- src/components/AppMusicList.vue
+++ src/components/AppMusicList.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="music-list">
     <div class="container">
-      <div class="row row-cols-5">
+      <div class="row row-cols-5" v-if="musicData.success">
         <MusicCard v-for="(item, index) in musicData.response" :key="index" :musicObj="item"/>
+      </div>
+      <div v-else class="loading-screen d-flex justify-content-center">
+        <font-awesome-icon icon="fas fa-spinner" />
       </div>
     </div>
   </div>
@@ -38,6 +41,39 @@ export default {
 
 .music-list{
   width: 80%;
-}
 
+  .loading-screen{
+    font-size: 3rem;
+    color: white;
+
+      svg{
+        animation-timing-function: linear;
+        animation-name: loaderRotation;
+        animation-duration: 1s;
+        animation-iteration-count: infinite;
+      }
+  }
+}
+@keyframes loaderRotation {
+  0%{
+    transform: rotate(0deg);
+  }
+
+  25%{
+    transform: rotate(90deg);
+  }
+
+  50%{
+    transform: rotate(180deg);
+    font-size: 150%;
+  }
+
+  75%{
+    transform: rotate(270deg);
+  }
+
+  100%{
+    transform: rotate(360deg);
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//font awesome
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faSpinner)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
 Vue.config.productionTip = false
 
 new Vue({
