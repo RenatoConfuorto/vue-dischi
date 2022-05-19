a5304a3689727aa52a76788cd66f06575861fa79
RenatoConfuorto <renato.confuorto@outlook.it>
2022-05-19 14:56:43 +0200

    aggiunto header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <AppHeader/>
+    </header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    AppHeader,
+    }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./style/style.scss";
+@import "./style/variables.scss";
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    <div class="logo-wrapper">
+      <img src="../assets/img/spotify-logo.svg" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../style/variables.scss";
+
+  .header{
+    width: 100%;
+    height: $app-header-height;
+    background-color: $app-secondary-color;
+
+    .logo-wrapper{
+      width: $app-header-height;
+      height: $app-header-height;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      img{
+        height: 70%;
+        width: auto;
+      }
+    }
+  }
+
+</style>
